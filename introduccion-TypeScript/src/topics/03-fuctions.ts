034c18c4fb9b465c00a06a0e5eba6af854e8696e
fserech <serech63@gmail.com>
2025-08-17 14:44:32 -0600

    clases de funciones con arguments

--- introduccion-TypeScript/src/topics/03-fuctions.ts
+++ introduccion-TypeScript/src/topics/03-fuctions.ts
@@ -13,8 +13,31 @@ function multiply(firstNumber: number, secondNumeber?: number, base:number=2) { 
 }
 
 
-const result: number = addNumbers(1, 2)
-const result2: string = addNumbersArrows(1, 2);
-const multiplyResult: number = multiply(5);
-console.log({result, result2}); // undefined
+//const result: number = addNumbers(1, 2)
+//const result2: string = addNumbersArrows(1, 2);
+//const multiplyResult: number = multiply(5);
+//console.log({result, result2, multiplyResult}); // undefined
+
+//CLASE DE FUNCIONES CON OBJETOS COMO ARGUMENTOS
+interface Character {
+    name: string;
+    hp: number;
+    showHp: () => void;
+}
+const healCharacter = (character: Character, amount: number) => {
+    character.hp += amount;
+}
+
+const strider: Character = {
+    name: 'Strider',
+    hp: 50,
+    showHp() {
+        console.log(`Puntos de vida ${this.hp}`);
+    }
+}
+
+healCharacter( strider, 10);
+healCharacter(strider, 20);
+strider.showHp();
+
 export{}
