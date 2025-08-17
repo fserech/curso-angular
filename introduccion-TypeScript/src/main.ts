80896a2b0312ba58b93800983dd1d9b252f687df
fserech <serech63@gmail.com>
2025-08-17 16:51:19 -0600

    destructuracion de arreglos y objetos

--- introduccion-TypeScript/src/main.ts
+++ introduccion-TypeScript/src/main.ts
@@ -1,7 +1,8 @@
 import './style.css'
 //import './topics/1-basic-types.ts';
 //import './topics/02-objet-interface.ts';
-import './topics/04-homework-types';
+//import './topics/04-homework-types';
+import './topics/05-basic-destructuring';
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 app.innerHTML = `Hola mundo TypeScript!`;
