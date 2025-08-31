6769ce721a953ee600c3cb2c2ff3397f332635d5
fserech <serech63gmail.com>
2025-08-31 14:40:52 -0600

    señales CounterSignal

--- bases/src/app/pages/counter/counter.ts
+++ bases/src/app/pages/counter/counter.ts
@@ -1,22 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, signal } from '@angular/core';
 @Component({
   templateUrl: './counter.html',
-  styleUrls: [
-    './counter.css'
-  ]
+  styleUrls: ['./counter.css']
 })
 export class Counter {
-  counter = 10;
+  counter = 10;// Variable normal
+   counterSignal = signal(10); // Signal (reactivo)
 
+  // Incremento
   increaseBy(value: number) {
     this.counter += value;
+    this.counterSignal.update((current) => current + value);
   }
 
+  // Decremento
   decrement() {
     this.counter--;
+    this.counterSignal.update((current) => current - 1);
   }
+
+  // Reset
   reset() {
     this.counter = 0;
+    this.counterSignal.set(0);
   }
-  }
+}
+
