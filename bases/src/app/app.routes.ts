d9b5ab5052951540f790b8a377d7efbbe222f5c2
fserech <serech63gmail.com>
2025-09-13 13:57:31 -0600

    AGREAGNDO UN NUEVO MUDULO PARA INTERACTUAR CON SUBMODULOS

--- bases/src/app/app.routes.ts
+++ bases/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import path from 'path';
 import { Counter } from './pages/counter/counter';
 import { Hero } from './pages/hero/hero';
+import { Dragonball } from './pages/dragonball/dragonball';
 
 
 export const routes: Routes = [
@@ -12,6 +13,10 @@ export const routes: Routes = [
 {
   path: 'hero',
   component: Hero
+},
+{
+  path: 'dragonball',
+  component: Dragonball
 },
 // Redireccionamiento al inicio si la ruta no existe
 {
---
+++ bases/src/app/pages/dragonball/dragonball.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dragonball',
+  imports: [],
+  templateUrl: './dragonball.html',
+})
+export class Dragonball{
+
+}
