52b5f5a1072a5abbf3bbc9bce330042955a3a608
fserech <serech63gmail.com>
2025-09-13 11:58:25 -0600

    agregando RouterLinkActive

--- bases/src/app/components/shared/navbar/navbar.ts
+++ bases/src/app/components/shared/navbar/navbar.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   imports: [
-    RouterLink
+    RouterLink, RouterLinkActive
   ],
   templateUrl: './navbar.html',
 })
